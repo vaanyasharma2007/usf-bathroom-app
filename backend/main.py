962443c420606cd7b805e9a41f79976336921235
Vaanya Sharma <sharmavaanya91@gmail.com>
2026-07-09 23:17:14 +0530

    Add bathroom availability endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,28 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Welcome to the bathroom availability app!"}
+@app.get("/bathrooms")
+def get_bathrooms():
+
+    return [
+        {"id":1,
+        "building":"Castor hall",
+        "floor":1,
+        "gender":"women",
+        "area_type":"toilet",
+        "total_units":5,
+        "available_units":3,
+        "status":"available"},
+        {
+            "id":2,
+            "building":"Castor hall",
+            "floor":1,
+            "gender":"women",
+            "area_type":"shower",
+            "total_units":6,
+            "available_units":2,
+            "status":"available"
+        }
+
+    ]
+    
