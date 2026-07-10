e6ca20272f5d0d4ef4f5c0bf9e9476a304c94a80
Vaanya Sharma <sharmavaanya91@gmail.com>
2026-07-10 20:32:48 +0530

    Add bathroom status logic

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,16 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Welcome to the bathroom availability app!"}
+def get_status(available_units,is_out_of_service):
+    if is_out_of_service:
+        return "out_of_service"
+    if available_units>0:
+        return "available"
+    elif available_units==0:
+        return "occupied"
+    elif available_units<0:
+        return None
+    
 @app.get("/bathrooms")
 def get_bathrooms():
 
@@ -16,7 +26,7 @@ def get_bathrooms():
         "area_type":"toilet",
         "total_units":5,
         "available_units":3,
-        "status":"available"},
+        "status":get_status(3,False)},
         {
             "id":2,
             "building":"Castor hall",
@@ -25,7 +35,7 @@ def get_bathrooms():
             "area_type":"shower",
             "total_units":6,
             "available_units":2,
-            "status":"available"
+            "status":get_status(2,True)
         }
 
     ]
