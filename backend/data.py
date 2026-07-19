f4b2b4b6e97d65f22b144f76ff0d3c352733b643
Vaanya Sharma <sharmavaanya91@gmail.com>
2026-07-19 18:33:19 +0530

    Move mock bathroom data into separate module

---
+++ backend/data.py
@@ -0,0 +1,21 @@
+bathroom =[
+       {"id":1,
+        "building":"Castor hall",
+        "floor":1,
+        "gender":"women",
+        "area_type":"toilet",
+        "total_units":5,
+        "available_units":3,
+        "status":get_status(3,False)},
+        {
+            "id":2,
+            "building":"Castor hall",
+            "floor":1,
+            "gender":"women",
+            "area_type":"shower",
+            "total_units":6,
+            "available_units":2,
+            "status":get_status(2,False)
+        }
+    ]
+    
